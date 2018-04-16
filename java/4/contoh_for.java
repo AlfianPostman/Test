5960d518ffa3fa67c19b46ddcff8625e33e333ca
AlfianPostman <kinobabes@gmail.com>
2018-04-16 14:13:20 +0700

    asuu

---
+++ java/4/contoh_for.java
@@ -0,0 +1,20 @@
+public class contoh_for {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Perulangan for naik");
+		for (int n = 1; n <= 5; ++n ) {
+			
+			System.out.println("naik: " + n);
+
+		}
+		System.out.println("Perulangan for turun");
+		for (int n = 1; n <= 5; --n ) {
+			
+			System.out.println("turun: " + n);
+
+		}
+
+	}
+	
+}
